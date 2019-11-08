ce813995b5f5f2ff0a0845809220bc2a1bd2e9bf
Matthe Jacobs <m_jacobs@hotmail.nl>
2019-11-08 13:03:09 +0100

    gebruikers profiel verder uitgebreid

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,15 +2,26 @@
 
 @section('content')
 	<div class="col-sm-10">
-		<p>Dit is het profiel</p>
-
 		<div class="row">
-			<div class="col-sm-3">
+			<div class="col-sm-4">
 				{{-- Profile image --}}
-				<img src="/images/default.png" alt="Your Profile Image" class="profile-image">
+				<img src="/images/default.png" alt="Your Profile Image" class="profile-image rounded-circle p-1">
+				{{-- Social media links --}}
+				<p class="text-center social-media-links">
+					<a href=""><i class="fab fa-facebook"></i></a>
+					<a href=""><i class="fab fa-twitter-square"></i></a>
+					<a href=""><i class="fab fa-instagram"></i></a>
+				</p>
+
 			</div>
-			<div class="col-sm-9">
+			<div class="col-sm-7">
 				{{-- Username --}}
+				<h3>
+					{{ Auth::user()-> name}}
+					<small class="text-muted">{{Auth::user()-> email}}</small>
+				</h3>
+				<hr>
+				Most Recent Activity
 			</div>
 		</div>
 	</div>
