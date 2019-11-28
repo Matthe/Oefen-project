88e357ebcafbbe57e45ea376b2bd797feb359cff
Matthe Jacobs <m_jacobs@hotmail.nl>
2019-11-28 18:17:58 +0100

    post pagina aangemaakt

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,4 +1,4 @@
 <title>Mijn site @yield('title')</title>
-<link rel="stylesheet" href="css/app.css">
-<link rel="stylesheet" href="css/custom.css">
+<link rel="stylesheet" href="/css/app.css">
+<link rel="stylesheet" href="/css/custom.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
