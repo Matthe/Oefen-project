8ad0b7537f02f6ba68355f756754269bbd818054
Matthe Jacobs <m_jacobs@hotmail.nl>
2019-11-28 15:30:45 +0100

    postcontroller aangemaakt

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,21 @@
-@include('partials._head');
-<body>
-	@include('partials._navbar')
+<!DOCTYPE html>
+<html>
+	<head>
+		@include('partials._head')
+	</head>
+	<body>
+		@include('partials._navbar')
 
-	<div class="container">
-		@if(Request::is('/'))
-			@include('partials._showcase')
-		@endif
-		@include('partials._messages')
-		@yield('content')
-	</div>
+		<div class="container">
+			@if(Request::is('/'))
+				@include('partials._showcase')
+			@endif
+			@include('partials._messages')
+			@yield('content')
+		</div>
 
-	@include('partials._footer')
-</body>
+		@include('partials._footer')
+
+		@include('partials._javascript')
+	</body>
 </html>
--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,8 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Mijn site @yield('title')</title>
-	<link rel="stylesheet" href="css/app.css">
-	<link rel="stylesheet" href="css/custom.css">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
-</head>
+<title>Mijn site @yield('title')</title>
+<link rel="stylesheet" href="css/app.css">
+<link rel="stylesheet" href="css/custom.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
