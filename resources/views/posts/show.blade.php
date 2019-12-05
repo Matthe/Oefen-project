b9bb3b6ead70953aad6965171c50772a1050ce24
Matthe Jacobs <m_jacobs@hotmail.nl>
2019-12-05 16:33:57 +0100

    bij de post sidebar gemaakt met info/edit/delete

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,8 +4,39 @@
 
 @section('content')
 
-	<h1> {{ $post->title }} </h1>
+	<div class="row">
+		<div class="col-md-8">
+			<h1> {{ $post->title }} </h1>
 
-	<p class="lead"> {{ $post->body }}</p>
+			<p class="lead"> {{ $post->body }}</p>
+		</div>
+
+		<div class="col-md-4">
+			<div class="card card-body">
+				<dl class="dl-horizontal">
+					<dt>Created At:</dt>
+					<dd>{{ date('d M, Y H:i', strtotime($post->created_at))}}</dd>
+				</dl>
+
+				<dl class="dl-horizontal">
+					<dt>Last Updated:</dt>
+					<dd>{{ date('d M, Y H:i', strtotime($post->updated_at))}}</dd>
+				</dl>
+				<hr>
+
+				<div class="row">
+					<div class="col-md-6">
+						{!! Html::linkRoute('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
+					</div>
+					<div class="col-md-6">
+						{!! Html::linkRoute('posts.destroy', 'Remove', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
+					</div>
+				</div>
+
+			</div>
+		</div>	
+	</div>
+
+	
 
 @endsection
